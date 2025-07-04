d515253bc1a85fd76c83c0e616a3ce2188714462
Deborah Lyra <deborahp.lyra@gmail.com>
2025-07-03 21:42:51 -0300

    css change

--- src/containers/general/MessageButton.tsx
+++ src/containers/general/MessageButton.tsx
@@ -4,9 +4,9 @@ import { useTranslation } from "react-i18next";
 export function MessageButton() {
 
   const { t } = useTranslation('header');
-  
+
   return (
-    <div className="fixed bottom-4 right-8 bg-secondary text-white p-2 rounded-full cursor-pointer">
+    <div className="fixed bottom-4 right-8 bg-secondary text-white p-2 rounded-xl cursor-pointer">
       {t('message')}
     </div>
   )
