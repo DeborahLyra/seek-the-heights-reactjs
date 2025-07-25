97bae66c580892fc06c569d61b979d3820f2d027
Deborah Lyra <deborahp.lyra@gmail.com>
2025-07-25 10:11:34 -0300

    banner adjustments

--- src/containers/landing/Banner.tsx
+++ src/containers/landing/Banner.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import imgBanner from '../../../public/img/imagemBanner.jpeg'
+import bgImage from '../../../public/img/imagemBanner.jpeg'
 
 export function Banner() {
 
@@ -8,15 +8,15 @@ export function Banner() {
     return (
         <section
             id='banner'
-            className="bg-primary text-secondary p-4 md:p-8 flex items-center justify-between flex-col gap-8 md:flex-row"
+            className="h-[40rem] bg-cover bg-bottom text-white p-4 md:p-8 flex items-center  flex-col gap-8 md:flex-row"
+            style={{ backgroundImage: `url(${bgImage})` }}
         >
             <div className=''>
                 <h1 className='text-5xl font-bold text-center md:text-left mb-4'>{t("banner.title")}</h1>
                 <p className='text-center text-xl md:text-left'>{t("banner.subtitle")}</p>
             </div>
 
-            <img src={imgBanner} alt="" className='h-fit md:h-80 lg:h-96 rounded-4xl'/>
-
+         
         </section>
     )
 }
