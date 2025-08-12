af8e6dad73dc06cd8a70ef01387c738dd6f47144
Deborah Lyra <deborahp.lyra@gmail.com>
2025-08-12 18:19:59 -0300

    edited: banner

--- src/containers/landing/Banner.tsx
+++ src/containers/landing/Banner.tsx
@@ -1,22 +1,23 @@
 import { useTranslation } from 'react-i18next';
-import bgImage from '../../../public/img/imagemBanner.jpeg'
+import bgImage from '../../../public/img/imagemBanner.jpeg';
 
 export function Banner() {
-
     const { t } = useTranslation('header');
 
     return (
         <section
             id='banner'
-            className="h-[40rem] bg-cover bg-bottom text-light-gray p-4 md:p-8 flex items-center  flex-col gap-8 md:flex-row"
+            className="h-[20rem] md:h-[40rem] bg-cover bg-bottom text-light-gray p-4 md:p-8 flex flex-col gap-8 justify-center md:justify-start"
             style={{ backgroundImage: `url(${bgImage})` }}
         >
-            <div className=''>
-                <h1 className='text-5xl font-bold text-center md:text-left mb-4'>{t("banner.title")}</h1>
-                <p className='text-center text-xl md:text-left'>{t("banner.subtitle")}</p>
+            <div className="text-center md:text-left">
+                <h1 className="md:mt-24 text-5xl md:text-6xl font-extrabold mb-4 text-white drop-shadow-lg">
+                    {t("banner.title")}
+                </h1>
+                <p className="text-xl md:text-2xl text-white/90 font-medium">
+                    {t("banner.subtitle")}
+                </p>
             </div>
-
-         
         </section>
-    )
+    );
 }
